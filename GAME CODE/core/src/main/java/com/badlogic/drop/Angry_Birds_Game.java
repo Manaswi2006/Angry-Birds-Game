40286c39d2aef624b23db687e4dfa9994d6e5223
Manaswiii <159019717+Manaswi2006@users.noreply.github.com>
2024-10-20 10:39:37 +0530

    Added the first screen
    Without buttons :)

--- GAME CODE/core/src/main/java/com/badlogic/drop/Angry_Birds_Game.java
+++ GAME CODE/core/src/main/java/com/badlogic/drop/Angry_Birds_Game.java
@@ -1,11 +1,42 @@
 package com.badlogic.drop;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Angry_Birds_Game extends Game {
+
+    //ATTRIBUTES
+    private SpriteBatch batch;
+    private static final int V_WIDTH = 1792;
+    private static final int V_HEIGHT = 1024;
+
+    //GETTERS AND SETTERS
+    public SpriteBatch getBatch(){
+        return this.batch;
+    }
+
+    public void setBatch(SpriteBatch sb){
+        this.batch = sb;
+    }
+
+    public static int getvHeight() {
+        return V_HEIGHT;
+    }
+
+    public static int getvWidth() {
+        return V_WIDTH;
+    }
+
+    //METHODS
     @Override
     public void create() {
-        setScreen(new FirstScreen());
+        this.setBatch(new SpriteBatch());
+        setScreen(new FirstScreen(this));
     }
+
+    @Override
+    public void render(){
+        super.render();
+    }
+
 }
--- GAME CODE/core/src/main/java/com/badlogic/drop/FirstScreen.java
+++ GAME CODE/core/src/main/java/com/badlogic/drop/FirstScreen.java
@@ -1,9 +1,48 @@
 package com.badlogic.drop;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
-/** First screen of the application. Displayed after the application is created. */
+
 public class FirstScreen implements Screen {
+
+    //ATTRIBUTES
+    private Angry_Birds_Game game;
+    Texture texture;
+    private OrthographicCamera gamecam;
+    private Viewport gameport;
+
+    //CONSTRUCTOR
+    public FirstScreen(Angry_Birds_Game _game){
+        this.game = _game;
+        texture = new Texture("Background.png");
+        gamecam = new OrthographicCamera();
+        gameport = new FitViewport(game.getvWidth(),game.getvHeight(),gamecam);
+    }
+
+    //GETTERS AND SETTERS
+    public Angry_Birds_Game getGame(){
+        return game;
+    }
+
+    public void setGame(Angry_Birds_Game _game){
+        this.game = _game;
+    }
+
+    public Texture getTexture() {
+        return texture;
+    }
+
+    public void setTexture(Texture _texture) {
+        this.texture = _texture;
+    }
+
+    //METHODS
     @Override
     public void show() {
         // Prepare your screen here.
@@ -11,11 +50,17 @@ public class FirstScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        // Draw your screen here. "delta" is the time since last render in seconds.
+        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.getBatch().setProjectionMatrix(gamecam.combined);
+        game.getBatch().begin();
+        game.getBatch().draw(texture, 0, 0);
+        game.getBatch().end();
     }
 
     @Override
     public void resize(int width, int height) {
+        gameport.update(width,height);
         // Resize your screen here. The parameters represent the new window size.
     }
 
