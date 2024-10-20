59bc11f46aed440f02b16814d1b8d8d9618a1ff4
Manaswiii <159019717+Manaswi2006@users.noreply.github.com>
2024-10-20 12:12:23 +0530

    Added Settings screen and buttons to settings and level 1

--- GAME CODE/core/src/main/java/com/badlogic/drop/SettingScreen.java
+++ GAME CODE/core/src/main/java/com/badlogic/drop/SettingScreen.java
@@ -10,27 +10,21 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
-
-public class FirstScreen implements Screen {
+public class SettingScreen implements Screen {
 
     //ATTRIBUTES
     private Angry_Birds_Game game;
     Texture texture;
     private OrthographicCamera gamecam;
     private Viewport gameport;
-    private Rectangle settingsButtonBounds;
-    private Rectangle level1ButtonBounds;
-
 
     //CONSTRUCTOR
-    public FirstScreen(Angry_Birds_Game _game){
+    public SettingScreen(Angry_Birds_Game _game){
         this.game = _game;
-        texture = new Texture("Background.png");
+        texture = new Texture("SETTINGS.png");
         gamecam = new OrthographicCamera();
         gameport = new FitViewport(1792,1024,gamecam);
         gamecam.position.set(1792 / 2f, 1024 / 2f, 0);
-        settingsButtonBounds = new Rectangle(648,  1024 - 775 - 76, 495, 76);
-        level1ButtonBounds = new Rectangle(648, 1024 - 653 - 76, 495, 76);
     }
 
     //GETTERS AND SETTERS
@@ -53,29 +47,9 @@ public class FirstScreen implements Screen {
     //METHODS
     @Override
     public void show() {
-        // Prepare your screen here.
+
     }
 
-    private void handleInput() {
-        if (Gdx.input.isTouched()) {
-            float touchX = Gdx.input.getX();
-            float touchY = Gdx.input.getY();
-
-            // Convert to world coordinates (invert Y-axis)
-            touchY = gameport.getScreenHeight() - touchY;
-
-            // Check if the touch is within the settings button bounds
-            if (settingsButtonBounds.contains(touchX, touchY)) {
-                game.setScreen(new SettingScreen(game)); // Navigate to SettingScreen
-                dispose();
-            }
-            // Check if the touch is within the level1 button bounds
-            else if (level1ButtonBounds.contains(touchX, touchY)) {
-                game.setScreen(new Level1Screen(game)); // Navigate to Level1Screen
-                dispose();
-            }
-        }
-    }
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0,0,0,1);
@@ -84,7 +58,6 @@ public class FirstScreen implements Screen {
         game.getBatch().begin();
         game.getBatch().draw(texture, 0, 0);
         game.getBatch().end();
-        handleInput();
     }
 
     @Override
@@ -95,21 +68,21 @@ public class FirstScreen implements Screen {
 
     @Override
     public void pause() {
-        // Invoked when your application is paused.
+
     }
 
     @Override
     public void resume() {
-        // Invoked when your application is resumed after pause.
+
     }
 
     @Override
     public void hide() {
-        // This method is called when another screen replaces this one.
+
     }
 
     @Override
     public void dispose() {
-        // Destroy screen's assets here.
+
     }
 }
