46c82244ed76c2cd277dd9c9da10df37359f323f
Manaswiii <159019717+Manaswi2006@users.noreply.github.com>
2024-11-29 23:04:30 +0530

    Implemented level 2

--- GAME CODE/core/src/main/java/com/badlogic/drop/Sprites/Red_Bird.java
+++ GAME CODE/core/src/main/java/com/badlogic/drop/Sprites/Red_Bird.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 
 public class Red_Bird extends Bird{
-    private final double speed = 4.0;// Pigs collide with birds
+    private final double speed = 3.0;// Pigs collide with birds
 
     public Red_Bird(Angry_Birds_Game game, float x, float y) {
         super(game, x, y, "Red_Bird.png");
